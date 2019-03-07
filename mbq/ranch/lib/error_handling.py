c652ca5a226d2edd2e67650c89bcd70d17dce947
Taylor Burgess <tayloraburgess@users.noreply.github.com>
2019-03-07 12:56:41 -0500

    Handle `InterfaceError` (#21)
    * Handle `InterfaceError`
    
    * Use django.db.utils.InterfaceError
    
    * Use celery.app.control.shutdown()

--- mbq/ranch/lib/error_handling.py
+++ mbq/ranch/lib/error_handling.py
@@ -1,6 +1,9 @@
 import logging
 
+from django.db.utils import InterfaceError
+
 import rollbar
+from celery.app import control
 
 from .. import _collector
 
@@ -12,6 +15,13 @@ def log_errors_and_send_to_rollbar(func):
     def wrapper(*args, **kwargs):
         try:
             func(*args, **kwargs)
+        except InterfaceError:
+            # An `InterfaceError` likely indicates the underlying Django database connection has
+            # been lost for some reason. Django doesn't try to recconnect or exit gracefully, so
+            # send SIGTERM here
+            logger.exception("Database connection error")
+            rollbar.report_exc_info()
+            control.shutdown()
         except Exception:
             logger.exception("Ranch signal error")
             rollbar.report_exc_info()

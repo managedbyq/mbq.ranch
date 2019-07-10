369753b5d469834d1d8a498d0593d846d6d06db9
Taylor Burgess <tayloraburgess@gmail.com>
2019-07-10 16:38:35 -0400

    Handle `InterfaceError`s in `log_errors_and_send_to_rollbar`

--- mbq/ranch/lib/error_handling.py
+++ mbq/ranch/lib/error_handling.py
@@ -1,6 +1,6 @@
 import logging
 
-from django.db.utils import InterfaceError
+from django.db.utils import InterfaceError, OperationalError
 
 import rollbar
 from celery.app import control
@@ -15,10 +15,14 @@ def log_errors_and_send_to_rollbar(func):
     def wrapper(*args, **kwargs):
         try:
             func(*args, **kwargs)
-        except InterfaceError:
+        except (InterfaceError, OperationalError):
             # An `InterfaceError` likely indicates the underlying Django database connection has
             # been lost for some reason. Django doesn't try to recconnect or exit gracefully, so
-            # send SIGTERM here
+            # send SIGTERM here.
+            # Also, handle the MySQL-specific `OperationalError`, which indicates a few types
+            # of possible errors:
+            # https://dev.mysql.com/doc/connector-python/en/connector-python-api-errors-operationalerror.html
+            # As it also can mean a lost database connection, send SIGTERM.
             logger.exception("Database connection error")
             rollbar.report_exc_info()
             control.shutdown()

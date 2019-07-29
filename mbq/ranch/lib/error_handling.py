e18bdae14b671f959a67f5d61a9458f7fd9a9115
Taylor Burgess <tayloraburgess@gmail.com>
2019-07-29 16:08:02 -0400

    Use celery.app.control with current Celery app

--- mbq/ranch/lib/error_handling.py
+++ mbq/ranch/lib/error_handling.py
@@ -3,7 +3,7 @@ import logging
 from django.db.utils import Error
 
 import rollbar
-from celery.app import control
+from celery.app import app_or_default, control
 
 from .. import _collector
 
@@ -20,7 +20,8 @@ def log_errors_and_send_to_rollbar(func):
             # necessary in every case, but it is safer
             logger.exception("Database error")
             rollbar.report_exc_info()
-            control.shutdown()
+            controller = control.Control(app=app_or_default())
+            controller.shutdown()
         except Exception:
             logger.exception("Ranch signal error")
             rollbar.report_exc_info()

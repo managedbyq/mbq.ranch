7bf0753246aff968b785be65e862d1d199aa3d75
Matt Briancon <mattbriancon@users.noreply.github.com>
2019-08-10 16:55:03 -0400

    Stop logging and track error-types in rmq stats

--- mbq/ranch/stats/rabbitmq.py
+++ mbq/ranch/stats/rabbitmq.py
@@ -17,18 +17,29 @@ def send_rabbitmq_queue_stats(broker_url, queue_names):
     username = parsed.username or "guest"
     password = parsed.password or "guest"
 
+    error_type = None
     try:
         response = requests.get(url, auth=(username, password), timeout=0.1)
-    except requests.exceptions.RequestException:
+    except requests.exceptions.Timeout:
         # We set a very aggressive timeout on the request so that we do not
         # block our worker for very long. It's expected that this will
         # sometimes fail, but it's unlikely to matter since we should be
         # collecting queue stats frequently. As such, we log but otherwise
         # swallow the exception (so we do not Rollbar or emit generic error
         # metrics)
-        logger.info("Error requesting metrics from RabbitMQ, skipping")
-        _collector.increment("rabbitmq.stats.timeout", value=1)
-        return
+        error_type = 'timeout'
+    except requests.exceptions.ConnectionError:
+        error_type = 'connection-error'
+    except requests.exceptions.HTTPError:
+        error_type = 'http-error'
+    except requests.exceptions.RequestException:
+        error_type = 'request-exception'
+    finally:
+        if error_type:
+            # backwards compatible. remove "rabbitmq.stats.timeout" after 1 December 2019
+            _collector.increment("rabbitmq.stats.timeout")
+            _collector.increment("rabbitmq.stats.error", tags={'error-type': error_type})
+            return
 
     response.raise_for_status()
 
